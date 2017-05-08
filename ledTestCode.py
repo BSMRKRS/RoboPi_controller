3bc694a904ed98964858bd0b5d07a5235486cb34
rickbo2 <jwrickman18@bsmschool.org>
2017-05-08 15:24:58 -0500

    Fixed issues with conflict merge by commenting out HEAD messages. Need a better fix

--- ledTestCode.py
+++ ledTestCode.py
@@ -18,26 +18,25 @@ def blink():
     time.sleep(0.5)
     print "led OFF"
 i = int(raw_input("How many times the light will blink>"))
-<<<<<<< HEAD
+#<<<<<<< HEAD
 #for n in range(i+1):
     #blink()
 
-def runServo():
-    RPL.servoWrite(ledPin,3000)
-=======
-for n in range(i):
-    blink()
+#def runServo():
+#    RPL.servoWrite(ledPin,3000)
+#=======
+#for n in range(i):
+ #   blink()
 
 def runServo():
     RPL.servoWrite(ledPin,250)
->>>>>>> 3ac775063ea8cbb37bfec0d8876d97d7aa6310ac
+#>>>>>>> 3ac775063ea8cbb37bfec0d8876d97d7aa6310ac
     print "led On"
     print "motor On"
     time.sleep(2)
     RPL.servoWrite(ledPin,0)
-<<<<<<< HEAD
     print "motor and ledOff"
 runServo()
-=======
+
 #runServo()
->>>>>>> 3ac775063ea8cbb37bfec0d8876d97d7aa6310ac
+#>>>>>>> 3ac775063ea8cbb37bfec0d8876d97d7aa6310ac
