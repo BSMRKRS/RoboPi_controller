52ccabefe84124bb4baba81aa18e14bd437d3763
Jack <jwrickman18@bsmschool.org>
2017-09-19 13:53:58 -0500

    Added comments to separate pin assignments from commands. Astetic

--- ledTestCode.py
+++ ledTestCode.py
@@ -2,10 +2,13 @@ import RoboPiLib_pwm as RPL
 import time as time
 RPL.RoboPiInit("/dev/ttyAMA0",115200)
 
+#pins
 ledPin = 1
-
 RPL.pinMode(ledPin,OUTPUT)
 
+
+
+#commands
 def blink():
     RPL.digitalWrite(ledPin,1)
     time.sleep(2)
