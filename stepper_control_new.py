965456c527bec8e02668f36cd0a1c0179d522a00
skinney-leonhardt19 <33100407+skinney-leonhardt19@users.noreply.github.com>
2018-11-26 10:20:45 -0600

    Update stepper_control_new.py

--- stepper_control_new.py
+++ stepper_control_new.py
@@ -38,7 +38,7 @@ def shoulder(dir, run_for = 1, speed = speed):
 
 
 def elbow(dir, run_for = 1, speed = speed):
-  run_for = (run_for * 1.862)
+  run_for = (run_for * 3.724)
   if(dir):
     RPL.digitalWrite(elbow_dir, 1)
   else:
